9e96ef35a058e99af581e99d74d2bd29eeb7d551
Tony Hallett <tonyhallett74@gmail.com>
2026-03-05 12:18:12 +0000

    minecart killer test - marker block for redstone replacement

--- src/gametest/java/tonyhallett/demomod/ServerMinecartKillerTest.java
+++ src/gametest/java/tonyhallett/demomod/ServerMinecartKillerTest.java
@@ -1,28 +1,13 @@
 package tonyhallett.demomod;
 
-import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
 import net.minecraft.item.Items;
 import net.minecraft.test.TestContext;
 
-import java.lang.reflect.Method;
-
-public class ServerMinecartKillerTest  implements CustomTestMethodInvoker {
-    @Override
-    public void invokeTestMethod(TestContext context, Method method) throws ReflectiveOperationException {
-        method.invoke(this, context);
-    }
-
+public class ServerMinecartKillerTest extends BaseServerTest {
     @SuppressWarnings({"unused"})
-    @GameTest(structure = "demomod:minecart_killer_function_test", maxTicks = 100)
-
+    @GameTest(structure = "demomod:minecart_killer_function_test")
     public void minecartKillerTest(TestContext context) {
-        var testBox = context.getTestBox();
-        var world = context.getWorld();
-
-        // minecart killer
-        context.runAtTick(99, () -> context.expectItem(Items.MINECART));
+        context.addInstantFinalTask(() -> context.expectItem(Items.MINECART));
     }
-
-
 }
---
+++ src/gametest/java/tonyhallett/demomod/TestStructureBlockFinder.java
@@ -0,0 +1,31 @@
+package tonyhallett.demomod;
+
+import net.minecraft.block.Block;
+import net.minecraft.test.TestContext;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestStructureBlockFinder
+{
+    private final TestContext _context;
+
+    public TestStructureBlockFinder(TestContext context){
+        _context = context;
+    }
+
+    public List<BlockPos> findBlocksInTestStructure(Block block){
+        List<BlockPos> results = new ArrayList<>();
+        _context.forEachRelativePos(pos -> {
+            var state = _context.getBlockState(pos);
+            if (state.isOf(block)) {
+                results.add(pos.mutableCopy());
+            }
+        });
+
+
+        return results;
+    }
+
+}
