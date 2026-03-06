52876774d1123338117a3244b1b3c91f06b64535
Tony Hallett <tonyhallett74@gmail.com>
2026-03-06 14:06:39 +0000

    add insert test and remove incorrect collision test - don't know a block based setup for collision aside from summon command block y 0.2

--- src/gametest/java/tonyhallett/demomod/ServerHopperPipeTest.java
+++ src/gametest/java/tonyhallett/demomod/ServerHopperPipeTest.java
@@ -3,11 +3,16 @@ package tonyhallett.demomod;
 import java.lang.reflect.Method;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.inventory.Inventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.test.TestContext;
 
 import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
 import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 
@@ -37,14 +42,15 @@ public class ServerHopperPipeTest implements CustomTestMethodInvoker {
     }
 
     private Runnable setupForCollision(TestContext context,boolean isHopperPipe){
+        var spawnItem = Items.MINECART;
         var hopperPos = new BlockPos(isHopperPipe ? 0 : 3,0,0);
         HopperCreator.createHopperBlockedFromAbove(context, hopperPos, isHopperPipe);
         /*
             public interface Hopper extends Inventory {
 	            Box INPUT_AREA_SHAPE = (Box)Block.createColumnShape(16.0, 11.0, 32.0).getBoundingBoxes().get(0);
         */
-        context.spawnItem(Items.MINECART, new Vec3d(hopperPos.getX() + 0.5,  hopperPos.getY() + 0.7, hopperPos.getZ() + 0.5));
-        return getHopperExpectation(context, isHopperPipe, hopperPos);
+        context.spawnItem(spawnItem, new Vec3d(hopperPos.getX() + 0.5,  hopperPos.getY() + 0.7, hopperPos.getZ() + 0.5));
+        return getHopperExpectation(context, isHopperPipe, hopperPos, spawnItem);
     }
 
     @SuppressWarnings({"unused"})
@@ -61,13 +67,14 @@ public class ServerHopperPipeTest implements CustomTestMethodInvoker {
     }
 
     private Runnable setupForExtract(TestContext context, boolean isHopperPipe){
+        var spawnItem = Items.MINECART;
         var hopperPos = new BlockPos(isHopperPipe ? 0 : 3,0,0);
         HopperCreator.createHopperBlock(context, hopperPos, isHopperPipe);
-        context.spawnItem(Items.MINECART, hopperPos.up());
-        return getHopperExpectation(context, isHopperPipe, hopperPos);
+        context.spawnItem(spawnItem, hopperPos.up());
+        return getHopperExpectation(context, isHopperPipe, hopperPos, spawnItem );
     }
 
-    private Runnable getHopperExpectation(TestContext context, boolean isHopperPipe, BlockPos hopperPos){
+    private Runnable getHopperExpectation(TestContext context, boolean isHopperPipe, BlockPos hopperPos, Item item){
         return () -> {
             if (isHopperPipe)
             {
@@ -75,10 +82,34 @@ public class ServerHopperPipeTest implements CustomTestMethodInvoker {
             }
             else
             {
-                context.expectContainerWithSingle(hopperPos,Items.MINECART);
+                context.expectContainerWithSingle(hopperPos, item);
             }
         };
     }
+    @GameTest
+    @SuppressWarnings({"unused"})
+    public void hopperPipePushes(TestContext context){
+        var inventory1 = createHopperPipeVerticalChain(context, true);
+        var inventory2 = createHopperPipeVerticalChain(context, false);
+        context.addInstantFinalTask(() -> {
+            context.assertFalse(inventory1.isEmpty(), Text.literal("hopper pipe should have pushed"));
+            context.assertFalse(inventory2.isEmpty(), Text.literal("hopper should have pushed"));
+        });
+    }
+
+    private Inventory createHopperPipeVerticalChain(TestContext context, boolean isHopperPipe){
+        var insertChestPos = new BlockPos(isHopperPipe? 0 : 3,0,0);
+        context.setBlockState(insertChestPos, Blocks.CHEST);
+        var insertChestBlockEntity = context.getBlockEntity(insertChestPos, ChestBlockEntity.class);
+        HopperCreator.createHopperBlock(context, insertChestPos.up(),isHopperPipe);
+        HopperCreator.createHopperBlock(context, insertChestPos.up(2),false);
+        var extractInventoryPos = insertChestPos.up(3);
+        context.setBlockState(extractInventoryPos, Blocks.CHEST);
+        var extractChestBlockEntity = context.getBlockEntity(extractInventoryPos, ChestBlockEntity.class);
+        extractChestBlockEntity.setStack(0, new ItemStack(Items.MINECART));
+        return insertChestBlockEntity;
+    }
+
 }
 
 class HopperCreator
